6c91da058bb793fd87eae6ea9ced74a6ac1009db
huyunying <huyunying@banggood.com>
2022-05-06 11:06:41 +0800

    压缩文件修改

--- src/packages/files-zip.ts
+++ src/packages/files-zip.ts
@@ -3,19 +3,19 @@ import JSZip from "jszip";
 import FileSaver from "file-saver";
 
 /**
- * 单个文件参数
  * prefix:每个文件下载路径前缀
  * fileName:压缩文件名
  * suffix:压缩文件后缀
+ * fileList:压缩文件数组
  */
 interface DownLoadConfig {
   prefix?: string;
   fileName?: string;
   suffix?: string;
-}
-interface fileListConfig {
-  fileUrl: string;
-  name?: string;
+  fileList: Array<{
+    fileUrl: string;
+    name?: string;
+  }>;
 }
 
 const DEFAULT_VALUE = {
@@ -33,51 +33,54 @@ function getFile(url) {
       .then((res) => {
         const { status, msg } = res;
         if (/^[4,5]/.test(status.toString())) {
-          reject(msg);
+          reject({
+            msg: msg,
+            url: url,
+          });
         }
         resolve(res);
       })
       .catch((error) => {
-        reject(error.msg || error.message);
+        const err = {
+          msg: error.msg || error.message,
+          url: url,
+        };
+        reject(err);
       });
   });
 }
-export default function filesZip(
-  fileList: Array<fileListConfig>,
-  options: DownLoadConfig
-) {
-  const { prefix, fileName, suffix } = setDefalut(options, DEFAULT_VALUE);
+export default function filesZip(options: DownLoadConfig) {
+  const { prefix, fileName, suffix, fileList } = setDefalut(
+    options,
+    DEFAULT_VALUE
+  );
   let zip = new JSZip();
   let promiseList = [];
   for (let item of fileList) {
     const url = prefix ? `${prefix}${item.fileUrl}` : item.fileUrl;
-    const promise = new Promise((resolve, reject) => {
-      getFile(url)
-        .then((res: any) => {
-          // 下载文件, 并存成文件流对象
-          const { headers, data } = res;
-          let file_name = "";
-          if (item["name"]) {
-            file_name = `${new Date().getTime()}${item.name}`;
-          } else {
-            const disposition = headers.get("content-disposition");
-            if (disposition && disposition.indexOf("attchement") !== -1) {
-              let filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
-              let matches = filenameRegex.exec(disposition);
-              if (matches != null && matches[1]) {
-                file_name = decodeURI(matches[1].replace(/['"]/g, ""));
-              }
-            } else {
-              reject("获取服务端返回的文件名失败");
-            }
+    const promise = getFile(url).then((res: any) => {
+      // 下载文件, 并存成文件流对象
+      const { headers, data } = res;
+      let file_name = "";
+      if (item["name"]) {
+        file_name = `${new Date().getTime()}${item.name}`;
+      } else {
+        const disposition = headers.get("content-disposition");
+        if (disposition && disposition.indexOf("attchement") !== -1) {
+          let filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+          let matches = filenameRegex.exec(disposition);
+          if (matches != null && matches[1]) {
+            file_name = decodeURI(matches[1].replace(/['"]/g, ""));
           }
-          zip.file(file_name, data, { binary: true }); // 逐个添加文件
-          resolve(true);
-        })
-        .catch((err) => {
-          reject(err);
-        });
+        } else {
+          // reject("获取服务端返回的文件名失败");
+          const arr = item.fileUrl.split("/");
+          file_name = arr[arr.length - 1];
+        }
+      }
+      zip.file(file_name, data, { binary: true }); // 逐个添加文件
     });
+
     promiseList.push(promise);
   }
 
